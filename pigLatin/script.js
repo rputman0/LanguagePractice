ad7ef32d167b2fa20b29f51be52f7450606424df
rputman0 <rputman0@gmail.com>
2017-11-14 20:03:43 -0500

    Change formatting

--- pigLatin/script.js
+++ pigLatin/script.js
@@ -1,7 +1,7 @@
 //translate a phrase of pig latin into english
 //"hetay uickqay rownbay oxfay" -> "the quick brown fox"
 let phrase = "hetay uickqay rownbay oxfay";
-let words = phrase.split(" ")
+let words = phrase.split(" ");
 
 for(let i=0;i<words.length;i++)
 	words[i] = words[i].charAt(words[i].length-3) + words[i].substring(0,words[i].length-3);
